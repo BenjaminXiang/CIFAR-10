b872210d7e42c520da086e0945e85ba310972f75
FranklinXiang <islongxiang@gmail.com>
2017-11-29 23:28:52 +0800

    Init

---
+++ lab8.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def train(images, one_hot_labels):
+    _num_images = len(images)
+    gray_images = 
+    raise NotImplementedError
+
+
+def predict(images):
+    # Return a Numpy ndarray with the length of len(images).
+    # e.g. np.zeros((len(images),), dtype=int) means all predictions are 'airplane's
+    raise NotImplementedError
